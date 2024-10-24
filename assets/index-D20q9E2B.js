f8cc8b6445f22cdc9f2f00148c04446b28cd00d4
Aadarsh Shrestha <aadarsh.stha36@gmail.com>
2024-10-24 18:11:11 +0545

    init

---
+++ assets/index-D20q9E2B.js
@@ -0,0 +1,1 @@
+import{d as a,j as o}from"./index-DyjPmHh2.js";const r=()=>{const{initData:t}=a();return console.log({initData:t}),o.jsx("div",{children:"Dashboard"})};export{r as default};
--- assets/index-Z5uheeo3.js
+++
@@ -1,1 +0,0 @@
-import{d as a,j as o}from"./index-KBM8R8LS.js";const r=()=>{const{initData:t}=a();return console.log({initData:t}),o.jsx("div",{children:"Dashboard"})};export{r as default};
